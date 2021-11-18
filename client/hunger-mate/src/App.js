ec5486cc48f5b8fc999005e9c7a1857c2e3392aa
Gbenga <olugbengaojo97@gmail.com>
2021-11-18 17:20:56 -0600

    warm up

--- client/hunger-mate/src/App.js
+++ client/hunger-mate/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [resourceType, setResourceType] = useState('posts');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    <div>
+      <button onClick={() => setResourceType('posts')}>Posts</button>
+      <button onClick={() => setResourceType('users')}>Users</button>
+      <button onClick={() => setResourceType('comments')}>Comments</button>
     </div>
+    <h1>{resourceType}</h1>
+   </>
   );
 }
 
