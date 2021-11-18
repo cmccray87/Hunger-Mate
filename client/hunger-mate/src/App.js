ea10338635b22a96ac5686e1681dec3c6b6dec04
Gbenga <olugbengaojo97@gmail.com>
2021-11-18 17:30:32 -0600

    header

--- client/hunger-mate/src/App.js
+++ client/hunger-mate/src/App.js
@@ -2,15 +2,12 @@ import React, { useState } from 'react'
 import './App.css';
 
 function App() {
-  const [resourceType, setResourceType] = useState('posts');
+  
   return (
    <>
-    <div>
-      <button onClick={() => setResourceType('posts')}>Posts</button>
-      <button onClick={() => setResourceType('users')}>Users</button>
-      <button onClick={() => setResourceType('comments')}>Comments</button>
-    </div>
-    <h1>{resourceType}</h1>
+   <header>
+     Hunger Mate
+   </header>
    </>
   );
 }
