433d9e29566b0d79a4d43ef0a60ff84859fe906a
David Crane <ddcrane2@gmail.com>
2021-11-17 20:26:26 -0600

    Nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,56 +1,51 @@
-import React from "react";
-import Auth from "../../utils/auth";
-import { Link } from "react-router-dom";
-
-
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav() {
+  
 
-    function showNavigation() {
-      if (Auth.loggedIn()) {
-        return (
-          <ul className="flex-row">
-            <li className="mx-1">
-              <Link to="/orderHistory">
-                Order History
-              </Link>
-            </li>
-            <li className="mx-1">
-              <a href="/" onClick={() => Auth.logout()}>
-                Logout
-              </a>
-            </li>
-          </ul>
-        );
-      } else {
-        return (
-          <ul className="flex-row">
-            <li className="mx-1">
-              <Link to="/signup">
-                Signup
-              </Link>
-            </li>
-            <li className="mx-1">
-              <Link to="/login">
-                Login
-              </Link>
-            </li>
-          </ul>
-        );
-      }
-    }
-  
-    return (
-      <header className="flex-row px-1">
-        <h1>
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
+
+  return (
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
           Hunger-Mates
-        </h1>
-  
-        <nav>
-          {showNavigation()}
-        </nav>
-      </header>
-    );
-  }
-  
-  export default Nav;
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+              About me
+            </a>
+          </li>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && !contactSelected && 'navActive'
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                  setContactSelected(false);
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
