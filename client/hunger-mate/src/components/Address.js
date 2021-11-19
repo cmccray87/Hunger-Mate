00a047481dcc0978f3823062af6458e7e800d678
Gbenga <olugbengaojo97@gmail.com>
2021-11-18 18:57:32 -0600

    error when adding components

---
+++ client/hunger-mate/src/components/Address.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Address() {
+    return (
+        <>
+        <div>
+            <h1>
+      Address
+      <input>Street Address</input>
+      <input>City</input>
+      <input>State</input>
+      <input>Zip Code</input>
+    </h1>
+        </div>
+        </>
+    )
+}
---
+++ client/hunger-mate/src/components/Send.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Send() {
+    return (
+    <>
+    <div>
+    <h1>Send Money</h1>
+     <input>Card Number</input>
+     <input>Name on Card</input>
+     <input>Security Code</input>
+     <input>Expiration Date</input>
+    </div>
+    </>
+    )
+}
