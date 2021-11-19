507ba321f9740ad526057a5f68ceb7c486f121a1
Gbenga <olugbengaojo97@gmail.com>
2021-11-19 15:04:35 -0600

    client

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hunger Mate</h1>
+      <h2>Address</h2>
+      <h3>Payout</h3>
+    </div>
+  );
+}
+
+export default App;
