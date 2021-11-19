7d868132b5ab79835500c43eac0dd7b4890da0e4
Gbenga <olugbengaojo97@gmail.com>
2021-11-19 15:22:21 -0600

    Aligned input bars

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,10 @@ function App() {
     <div>
       <header>Hunger Mate</header>
       <h1>Address</h1>
-      <input type="text"></input>
+      <input type="text" id="where"></input>
       <button>Submit</button>
       <h1>Payout</h1>
-      <input type="text"></input>
+      <input type="text" id="how"></input>
       <button>Submit</button>
     </div>
   );
