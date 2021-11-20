e777e37ffaccd6f25ab9dbf3f6612676e6eaa9d2
Gbenga <olugbengaojo97@gmail.com>
2021-11-20 15:35:51 -0600

    Logged in Successfully

--- client/src/App.js
+++ client/src/App.js
@@ -15,8 +15,14 @@ const [error, setError] = useState("");
 const Login = details => {
   console.log(details);
 
-  if (details.email == adminUser.email && details.password == adminUser.password)
+  if (details.email == adminUser.email && details.password == adminUser.password) {
   console.log("Logged in");
+  setUser({
+    name: details.name,
+    email: details.email
+  });
+}
+
  else {
   console.log("Details do not match")
   }
