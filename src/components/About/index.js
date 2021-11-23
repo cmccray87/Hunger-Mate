52317368ee46ea3b35c82a21841fb13442b30be0
Missy Stiner <missystiner@gmail.com>
2021-11-22 20:16:32 -0600

    About us description updated

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,12 +8,16 @@ function About() {
         <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
         </div> */}
         <p>
-          For some it may be breakfast burritos, for others it might be tacos, and for very few it may be a banana.
-          You are hungover, and we are eager to help; let us help you. Order your most desired food and beverage combo
-          right here and we'll deliver it to you in just minutes! 
+        Rough night? You’ve come to the right place to start your recovery!
         </p>
         <p>
-        Echo Park sriracha Pinterest vegan biodiesel 90's irony iPhone Kickstarter Carles crucifix kitsch narwhal dreamcatcher pickled trust fund selvage art party letterpress Tumblr post-ironic kogi Thundercats Tonx Brooklyn Pitchfork Odd Future authentic normcore freegan leggings Schlitz chambray organic tousled retro fap squid street art church-key fashion axe Wes Anderson bespoke whatever gentrify banh mi you probably haven't heard of them American Apparel next level pug ugh.
+        When someone says, “Beer me!”
+        you hand them a beer.
+        When someone says, “Feed me!”
+        you hand them Hunger-Mate!
+        </p>
+        <p>
+        Our options are simple with a one-click food and drink combo. Sign up today to get your order on the way!
         </p>
       </div>
     </section>
